5b63a863208988b686418aebe22248476bb1fa10
William Revah <wr@wrci.fr>
2014-09-12 13:01:19 +0200

    Mise en place des routes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+#Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( blueprint/screen.css )
+Rails.application.config.assets.precompile += %w( blueprint/print.css )
+Rails.application.config.assets.precompile += %w( blueprint/ie.css )
+Rails.application.config.assets.precompile += %w( custom.css )
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/contact'
-  
-  get 'pages/about'
-  
-  get 'pages/help'
+  get '/contact', :to => 'pages#contact'
+  get '/about',   :to => 'pages#about'
+  get '/help',    :to => 'pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
